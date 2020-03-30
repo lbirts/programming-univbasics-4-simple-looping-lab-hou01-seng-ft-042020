45525cddafc77dfe062145132dcdb0e501fe3fbd
Lauren Birts <birtslauren@gmail.com>
2020-03-30 20:05:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,3 +22,12 @@ def output_array(array)
     count += 1
   end
 end
+
+def return_string_array(array)
+  new_array = []
+  count = 0
+  while count < array.length
+    new_array.push(array[count].to_s)
+    count += 1
+  end
+end
